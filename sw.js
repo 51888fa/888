59823d09144edfb1242a52a8b4b16b6ae706d080
51888fa <18644465@qq.com>
2026-05-29 11:47:38 +0800

    Update sw.js service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11071503
+    "domain": "5gvci.com",
+    "zoneId": 11071770
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
